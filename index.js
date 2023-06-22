ffe4bd6a2732590c94611ad6747abc777595ecbf
Peter Lee <wonjonglee@rivian.com>
2023-06-22 14:19:51 -0500

    chip8+cool backdrop

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ for (let i = 0; i < $hoverables.length; i++) {
 // Move the cursor
 function onMouseMove(e) {
   TweenMax.to($bigBall, .4, {
-    x: e.pageX - 15,
-    y: e.pageY - 15
+    x: e.pageX - 23,
+    y: e.pageY - 22
   })
   TweenMax.to($smallBall, .1, {
-    x: e.pageX - 5,
-    y: e.pageY - 42
+    x: e.pageX - 13,
+    y: e.pageY - 49
   })
 }
 
